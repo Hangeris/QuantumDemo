3920469bacf676a0a724c76cfc3e59b0da09ac9d
Hangeris <liudas.spangelevicius@gmail.com>
2024-08-10 10:19:26 +0300

    Create player RespawnSystem.cs

---
+++ quantum_code/quantum.code/Systems/Respawn/RespawnSystem.cs
@@ -0,0 +1,17 @@
+﻿using Quantum.Systems.Player;
+
+namespace Quantum.Systems.Respawn
+{
+    public unsafe class RespawnSystem : SystemMainThreadFilter<PlayerSystem.Filter>
+    {
+        public const int RespawnHeight = -10;
+        
+        public override void Update(Frame frame, ref PlayerSystem.Filter filter)
+        {
+            if (filter.Transform->Position.Y < RespawnHeight)
+            {
+                filter.Transform->Position = PlayerSpawner.SpawnSystem.GetSpawnPosition(filter.Link->Player);
+            }
+        }
+    }
+}
