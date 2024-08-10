8dd0c062caecb6a97f116753b453a849b1d18f13
Hangeris <liudas.spangelevicius@gmail.com>
2024-08-10 09:34:53 +0300

    Implement player spawning when entering from Menu.unity

--- quantum_unity/Assets/Photon/QuantumDemo/Menu/Scripts/CustomCallbacks.cs
+++ quantum_unity/Assets/Photon/QuantumDemo/Menu/Scripts/CustomCallbacks.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using Quantum;
+using UnityEngine;
 
 public class CustomCallbacks : QuantumCallbacks
 {
+    [Header("Reference")]
+    [SerializeField] private RuntimePlayer runtimePlayer;
+    
     public override void OnGameStart(Quantum.QuantumGame game)
     {
         if (game.Session.IsPaused) 
@@ -10,7 +14,7 @@ public class CustomCallbacks : QuantumCallbacks
         foreach (var localPlayer in game.GetLocalPlayers())
         {
             Debug.Log("CustomCallbacks - sending player: " + localPlayer);
-            game.SendPlayerData(localPlayer, new Quantum.RuntimePlayer { });
+            game.SendPlayerData(localPlayer, runtimePlayer);
         }
     }
 
